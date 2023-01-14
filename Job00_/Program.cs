b140d2af8d87e0d2beeea7026a83d4b9a900bdc6
Pradim22 <792616@mail.ru>
2023-01-14 23:15:55 +0400

    Job00_square number

--- Job00_/Program.cs
+++ Job00_/Program.cs
@@ -1,5 +1,10 @@
-﻿
-Console.Write("введите число: ");
-int numA = Console.ReadLine();
-
-
+﻿/* 
+Алгоритм:
+1. На вход принимает число
+2. рассчитывает квадрат этого числа
+3. вывод результат 
+*/
+Console.Write("введите целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine()); //команда Convert.ToInt32() конвертирует аргумент типа String в число типа Int
+int square = numA * numA;
+Console.WriteLine($"Квадрат числа {numA} = {square}"); //интерполяция строк
