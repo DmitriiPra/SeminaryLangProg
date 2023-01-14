06f26e297150de626d2e8a9bc02210fa658924fd
Pradim22 <792616@mail.ru>
2023-01-14 23:37:06 +0400

    Task01 Check numbers

---
+++ Task01/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Алгоритм
+1. ввод числа 1
+2. ввод числа 2
+3. проверка является ли первое число квадратом второго
+4. Вывод результата в консоль
+ */
+
+Console.Write("Введите значение числа 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение числа 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 == (number2 * number2))
+{
+    Console.WriteLine($"Число {number1} ЯВЛЯЕТСЯ квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} НЕ ЯВЛЯЕТСЯ квадратом числа {number2}");
+}
