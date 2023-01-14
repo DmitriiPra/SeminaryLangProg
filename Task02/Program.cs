46e837fc1fd56740215727f914d1783e87a2bf80
Pradim22 <792616@mail.ru>
2023-01-15 00:15:42 +0400

    Task02 numbers week in string

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -6,16 +6,16 @@
  */
 
 Console.Write("Введите целое число от 1 до 7, определяющее день недели: ");
-int number = Convert.ToInt32(Console.ReadLine());
+string number = Console.ReadLine();
 
-if(number == 1) Console.WriteLine("Число 1 это Понедельник");
-else if(number == 2) Console.WriteLine("Число 2 это Вторник");
-else if(number == 3) Console.WriteLine("Число 3 это Среда");
-else if(number == 4) Console.WriteLine("Число 4 это Четверг");
-else if(number == 5) Console.WriteLine("Число 5 это Пятница");
-else if(number == 6) Console.WriteLine("Число 6 это Суббота");
-else if(number == 7) Console.WriteLine("Число 7 это Воскресенье");
-else Console.WriteLine($"Вы ввели неверное значение! Число {number} НЕ ЯВЛЯЕТСЯ числом, определяющим день недели");
+if(number == "1") Console.WriteLine("Число 1 это Понедельник");
+else if(number == "2") Console.WriteLine("Число 2 это Вторник");
+else if(number == "3") Console.WriteLine("Число 3 это Среда");
+else if(number == "4") Console.WriteLine("Число 4 это Четверг");
+else if(number == "5") Console.WriteLine("Число 5 это Пятница");
+else if(number == "6") Console.WriteLine("Число 6 это Суббота");
+else if(number == "7") Console.WriteLine("Число 7 это Воскресенье");
+else Console.WriteLine("Вы ввели неверное значение!");
 
 
  
