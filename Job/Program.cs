a784c2597ef5379e4578d8e39c5410ceac8a7595
Pradim22 <792616@mail.ru>
2023-01-15 11:45:27 +0400

    переименовали папку в Task03, создали папку Task05

---
+++ Job/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+// int path = 100;
+// int minDist = 2;
+// int speedFrend1 = 1;
+// int speedFrend2 = 2;
+// int speedDog = 5;
+// int choiceFrend = 1; // у какого друга в настоящий момент собака
+// int count = 0;
+
+// while (path > minDist)
+// {
+//     if (choiceFrend == 1)
+//     {
+//         int timePath = path / (speedDog + speedFrend2);
+//         path = path - timePath * (speedFrend1 + speedFrend2);
+//         choiceFrend = 2;
+//     }
+//     else
+//     {
+//         int timePath = path / (speedDog + speedFrend1);
+//         path = path - timePath * (speedFrend1 + speedFrend2);
+//         choiceFrend = 1;
+//     }
+//     count++;
+// }
+Console.WriteLine("За время пока 2 друга идут на встречу друг другу, собака пробежит между ними {count} раз.");
