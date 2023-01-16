2350341ea429a2859674bafcda1443b7e7b03410
Pradim22 <792616@mail.ru>
2023-01-16 10:43:18 +0400

    JobDog написан код

--- JobDog/Program.cs
+++ JobDog/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int path = 100;
+int minDist = 2;
+int speedFriend1 = 1;
+int speedFriend2 = 2;
+int speedDog = 5;
+int choiceFriend = 1; // у какого друга в настоящий момент собака
+int count = 0;
+int timePath = 0;
+
+while (path > minDist)
+{
+    if (choiceFriend == 1)
+    {
+        timePath = path / (speedDog + speedFriend2);
+        path = path - timePath * (speedFriend1 + speedFriend2);
+        choiceFriend = 2;
+    }
+    else
+    {
+        timePath = path / (speedDog + speedFriend1);
+        path = path - timePath * (speedFriend1 + speedFriend2);
+        choiceFriend = 1;
+    }
+    count++;
+    //Console.WriteLine($"кол-во = {count}, расстояние между друзьями = {path}.");
+}
+Console.WriteLine($"За время пока 2 друга идут на встречу друг другу, собака пробежит между ними {count} раз.");
