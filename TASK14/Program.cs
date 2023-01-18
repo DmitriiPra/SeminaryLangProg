e67b7236a441ef35b342c0319b6241b646bba48f
Pradim22 <792616@mail.ru>
2023-01-18 14:01:49 +0400

    replace folder task14

---
+++ TASK14/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+принимает на вход число и 
+проверяет, кратно ли оно одновременно 7 и 23.
+*/
+
+bool CheckingMultiplicity(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckingMultiplicity(number);
+
+if (result == true) Console.WriteLine("КРАТНО");
+else Console.WriteLine($"НЕ КРАТНО");
