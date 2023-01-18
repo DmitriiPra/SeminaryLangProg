533488f6779508608d5c973b13aae141ccace23f
Pradim22 <792616@mail.ru>
2023-01-18 13:05:39 +0400

    Task11 ready

---
+++ TASK11/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+выводит случайное трёхзначное число и 
+удаляет вторую цифру этогочисла.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randomNum = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное число = {randomNum}");
+
+int NumberConversion(int num)
+{
+    return num / 100 * 10 + num % 10;
+}
+Console.WriteLine($"Преобразованное число = {NumberConversion(randomNum)}");
