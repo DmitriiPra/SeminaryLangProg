f5ba901f5aff40a3970a3892c4e2ab4823207301
Pradim22 <792616@mail.ru>
2023-01-18 14:36:00 +0400

    TASK16 ready

---
+++ TASK16/Program.cs
@@ -0,0 +1,16 @@
+﻿/* принимает на вход два числа и проверяет, 
+является ли одно число квадратом другого.
+*/
+
+bool CheckingMultiplicity(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.WriteLine("введите два числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+bool result = CheckingMultiplicity(numberA, numberB);
+if (result == true) Console.WriteLine("является");
+else Console.WriteLine("Не является");
