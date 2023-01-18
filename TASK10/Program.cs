2bf18382f11f5bef57bbdb7572f8f5b7623de02c
Pradim22 <792616@mail.ru>
2023-01-18 19:45:35 +0400

    Task10 ready

---
+++ TASK10/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 
+принимает на вход трёхзначное число и 
+на выходе показывает вторую цифру этого числа.
+*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondNum(int num)
+{
+    return num / 10 % 10;
+}
+
+int secondChar = SecondNum(number);
+Console.WriteLine($"Cредняя цифра = {secondChar}");
