7dca3dd697aaf5fe8b754931c7a029447224dc0b
Pradim22 <792616@mail.ru>
2023-01-25 13:13:35 +0400

    TASK17 Ready

---
+++ TASK17/Program.cs
@@ -0,0 +1,24 @@
+﻿// Принимает на вход координаты точки Х и У
+// Проверяет что Х и У не равны 0
+// Выводит номер четверти плоскости в которой находится точка
+
+Console.WriteLine("Введите координаты:");
+Console.Write("X = ");
+int coordX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y = ");
+int coordY = Convert.ToInt32(Console.ReadLine());
+
+int Index(int cX, int cY)
+{
+    if (cX > 0 && cY > 0) return 1;
+    if (cX < 0 && cY > 0) return 2;
+    if (cX < 0 && cY < 0) return 3;
+    if (cX > 0 && cY < 0) return 4;
+    return 0;
+}
+
+int index = Index (coordX, coordY);
+string result = index != 0 ? 
+        $"координаты относятся к {index} четверти" 
+        : "Х или У равно 0";
+Console.WriteLine(result);
