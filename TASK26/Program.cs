6e46af5e5f5dea3ffefd30991e2b27a62b9e859a
Pradim22 <792616@mail.ru>
2023-02-02 13:15:13 +0300

    Task26 ready

--- TASK26/Program.cs
+++ TASK26/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+принимает на вход число А
+выдает количество цифр в чисел А
+743 -> 3
+45 -> 2
+82542 -> 5
+*/
+
+Console.Write("Введите число А:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int Quantity(int a)
+{
+    if (a < 0) a *= -1;
+    int count = 0;
+    if (a == 0) return count =1;
+    for (int divider = 10; a > 0; count++)
+        a /= divider;
+    return count;
+}
+int quantity = Quantity(numberA);
+System.Console.WriteLine($"{numberA} -> {quantity}");
