e20ebd361e15cc54375585f6b2a21048cec29fe9
Pradim22 <792616@mail.ru>
2023-02-05 10:37:47 +0300

    task 28 ready

--- TASK28/Program.cs
+++ TASK28/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+принимает на вход число N
+выдает факториал числа N
+4 -> 24
+5 -> 120
+
+*/
+
+Console.Write("Введите число натуральное число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int a)
+{
+    int result = 1;
+    for (int i = 1; i <= a; i++) result *= i;
+    return result;
+}
+
+if (numberN > 0)
+{
+    int factorialN = Factorial(numberN);
+    Console.WriteLine($"Факториал {numberN} -> {factorialN}");
+}
+else Console.WriteLine("Введено неверное значение N");
