6a0a2c21133d69ef47fe84912dd9b246f91803d1
Pradim22 <792616@mail.ru>
2023-02-09 07:05:52 +0300

    task 30 ready

--- TASK30/Program.cs
+++ TASK30/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// выводит массив из 8 элементов наполненный случайно сгенерированными 0 и 1 
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr [i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[arr.Length - 1]}]");
+}
+
+int[] array = GenArray(8, 0, 1);
+PrintArray(array);
