ae21b266389c0c6a4350e5293409df86af74f350
Pradim22 <792616@mail.ru>
2023-02-10 23:42:11 +0300

    task27 ready

--- TASK27/Program.cs
+++ TASK27/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+принимает на вход число А
+выдает сумму цифр в числе А
+123 -> 6
+-45 -> 9
+*/
+
+Console.Write("Введите число А:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+//if (numberA < 0) numberA *= -1;
+
+int SummaNum(int a)
+{
+    if (a < 0) a *= -1;
+
+    int amount = 0;
+    while (a > 0)
+    {
+        amount += a % 10;
+        a /= 10;
+    }
+    return amount;
+}
+int summaNum = SummaNum(numberA);
+System.Console.WriteLine($"{numberA} -> {summaNum}");
