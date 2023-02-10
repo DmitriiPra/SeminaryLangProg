43dfde742ab000c7b8910d48b651f8d86f67cfe8
Pradim22 <792616@mail.ru>
2023-02-10 22:33:08 +0300

    task29 ready

--- TASK29/Program.cs
+++ TASK29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// выводит массив из 8 элементов и выводит их на экран:
+//12, 2, 46, 54, 36, 78, 6, 64 -> [12, 2, 46, 54, 36, 78, 6, 64] 
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr [i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write(arr[arr.Length - 1]);
+}
+
+int[] array = GenArray(8, 0, 99);
+PrintArray(array);
+Console.Write(" -> [");
+PrintArray(array);
+Console.WriteLine("]");
