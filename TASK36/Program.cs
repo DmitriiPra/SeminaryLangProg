b924373edea58ad146a9ed0633a620a994e1e822
Pradim22 <792616@mail.ru>
2023-02-11 12:51:57 +0300

    Task 36 ready

--- TASK36/Program.cs
+++ TASK36/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}] -> ");
+}
+void SummaEvenNumbers(int[] array)
+{
+    int summ = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) summ += array[i];
+    }
+    System.Console.WriteLine(summ);
+}
+
+int[] arr = GenArray(5, 1, 21);
+PrintArray(arr);
+SummaEvenNumbers(arr);
