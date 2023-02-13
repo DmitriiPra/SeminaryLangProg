78c050a77cf989e33d883b73644c05556862728d
Pradim22 <792616@mail.ru>
2023-02-13 23:37:43 +0300

    TASK 39 ready

---
+++ TASK39/Program.cs
@@ -0,0 +1,37 @@
+﻿// Программы переворачивающая одномерный массив
+//  [1 2 3 4 5 6] -> [6 5 4 3 2 1]
+
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}] ");
+}
+void MirrorArray(int[] arr)
+{
+    int temp = default;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+}
+int[] array = GenArray(5, -99, 100);
+PrintArray(array);
+Console.Write(" -> ");
+MirrorArray(array);
+PrintArray(array);
---
+++ TASK41/Program.cs
@@ -0,0 +1,38 @@
+﻿// Пользователь вводит М чисел
+// определить сколько из введенных чисел больше 0
+// 1, -20, 7, -2, -3 -> 2
+
+Console.WriteLine("Задайте количество обрабатываемых чисел: ");
+int sizeM = Convert.ToInt32(Console.ReadLine());
+
+int[] NumbersArray(int size)
+{
+    int[] numbersArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return numbersArray;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]} -> ");
+}
+void ResultCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    Console.WriteLine(count);
+}
+
+int[] numbersArray = NumbersArray(sizeM);
+PrintArray(numbersArray);
+ResultCount(numbersArray);
