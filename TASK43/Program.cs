ab19e620c528669b7592e01b8c8207c2b3778649
Pradim22 <792616@mail.ru>
2023-02-15 00:09:03 +0300

    TASK 43 ready

--- TASK43/Program.cs
+++ TASK43/Program.cs
@@ -2,11 +2,6 @@
 // y = k1 * x + b1 и y = k2 * x + b2
 // значения k1, b1, k2, b2 задаются пользователем
 // 
-//     k01 * x + b01 = k02 * x + b02 -> 
-//     k01x - k02x = b02 - b01
-//     x(k01-k02) = b02 - b01
-//     x = (b02 - b01)/(k01-k02)
-
 
 Console.WriteLine("Введите значение b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
@@ -26,23 +21,27 @@ void CrossCoordinates(double k01, double b01, double k02, double b02)
 }
 CrossCoordinates(k1, b1, k2, b2);
 
-/* решение через массив
+/*
+Решение через массив. 
+Не смог разобраться. Подскажите в чем проблема, выдает 2 ошибки: 
+ 1. Не удается неявно преобразовать тип "double[]" в "double". 
+ 2. Имя "coordinats" не существует в текущем контексте.
+
 double[] IntersectionPoint(double k01, double b01, double k02, double b02)
 {
     double[] coordinats = new double[2];
 
     coordinats[0] = (b02 - b01) / (k01 - k02);
-    coordinats[1] = k01 * ((b02 - b01) / (k01 - k02)) + b01;
+    coordinats[1] = k01 * coordinats[0] + b01;
     return coordinats;
 }
-// void PrintArray(double[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.Write($"{arr[i]}, ");
-//     }
-//     // Console.Write($"{arr[arr.Length - 1]}");
-// }
-
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
 double intersectionPoint = IntersectionPoint(k1, b1, k2, b2);
+PrintArray(coordinats);
 */
