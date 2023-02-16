5231404d6e5299c78ce9724f4b02cb36c95d3abb
Pradim22 <792616@mail.ru>
2023-02-16 16:00:54 +0300

    TASK 47 ready

---
+++ Shablon/Program.cs
@@ -0,0 +1,63 @@
+﻿// Одномерный массив
+int[] GenArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}] ");
+}
+
+// Заполнение одномерного массива вещественных чисел:
+
+double[] GenArray(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 1);
+    }
+    return arr;
+}
+
+// Двумерный массив
+
+int[,] GenMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 3} |");
+            else Console.WriteLine($"{matrix[i, j], 3} |");
+        }
+    }
+}
+int[,] genMatrix = GenMatrix(3, 4, 0, 10);
+PrintArray(genMatrix);
--- TASK47/Program.cs
+++ TASK47/Program.cs
@@ -9,3 +9,32 @@ m = 3, n = 4.
 
 8 7,8 -7,1 9
 */
+//arr[i] = Math.Round((rnd.NextDouble() * (max - min) + min), 1);
+
+double[,] GenMatrix(int rows, int columns, double min, double max)
+{
+    double[,] matrix = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((rnd.NextDouble() * (max-min) + min), 2);
+        }
+    }
+    return matrix;
+}
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($" {matrix[i, j], 5} |");
+            else Console.WriteLine($" {matrix[i, j], 5} |");
+        }
+    }
+}
+double[,] genMatrix = GenMatrix(3, 4, 0, 100);
+PrintArray(genMatrix);
