d892c7623bea0121c66e18312545f40c0c1fbaea
Pradim22 <792616@mail.ru>
2023-02-20 08:06:31 +0300

    TASK54 ready

--- TASK54/Program.cs
+++ TASK54/Program.cs
@@ -12,3 +12,55 @@
 8 4 4 2
 */
 
+int[,] GenMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}  ");
+            else Console.WriteLine($"{matrix[i, j],3}");
+        }
+    }
+}
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int n = 0; n < matrix.GetLength(1); n++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                int temp = default;
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] genMatrix = GenMatrix(3, 6, -9, 9);
+Console.WriteLine();
+PrintArray(genMatrix);
+SortMatrix(genMatrix);
+Console.WriteLine();
+PrintArray(genMatrix);
+Console.WriteLine();
