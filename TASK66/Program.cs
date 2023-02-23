991a27120e52934ced271c8b96aad8fe36d9bd23
Pradim22 <792616@mail.ru>
2023-02-23 08:40:20 +0300

    TASK 66 READY

--- TASK66/Program.cs
+++ TASK66/Program.cs
@@ -5,5 +5,32 @@
 Выполнить с помощью рекурсии.
 
 M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
+M = 4; N = 8 -> 30 = 4 + 5 + 6 + 7 + 8
+
 */
+
+int SummNaturalNumber(int m, int n)
+{
+    if (m > n)
+    {
+        if (n > m) return 0;
+        else return n + SummNaturalNumber(m, n + 1);
+    }
+    else if (m < n)
+    {
+        if (m > n) return 0;
+        else return m + SummNaturalNumber(m + 1, n);
+    }
+    return m;
+}
+
+Console.Write("Задайте значение натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значение натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if (numberM > 0 && numberN > 0)
+{
+    int summNaturalNumber = SummNaturalNumber(numberM, numberN);
+    Console.WriteLine($"M = {numberM}; N = {numberN} -> {summNaturalNumber}");
+}
+else Console.WriteLine("Введено некорректное значение!");
